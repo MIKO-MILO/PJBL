1389c54cdc614181c9840f6de6de080a0c046a4a
MIKO-MILO <168565389+MIKO-MILO@users.noreply.github.com>
2024-11-20 07:48:02 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,46 @@
+<div class="footer" style="padding-top: 20vw;">
+<header class="bg-secondary py-3 text-center">
+    <div class="container d-flex align-items-center justify-content-center">
+      <img src="FOTO/picture1_2.png" alt="Logo" class="logo-image me-2" />
+      <h1 class="mb-0" style="font-size: 2vw; color: #ffffff"><b>SMPN 4 Malang</b></h1>
+    </div>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-md navbar-light">
+      <div class="container">
+        <div class="collapse navbar-collapse justify-content-center">
+          <ul class="navbar-nav">
+            <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Accommodation</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Gallery</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Local Attraction</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Social Media Icons -->
+  <div class="text-center mt-3">
+    <a href="https://www.facebook.com/arvegatu.esempeempat" target="_blank" class="mx-2 text-dark" style="font-family: 'Poppins'"><i class="bi bi-facebook"></i></a>
+    <a href="https://www.instagram.com/smpn4.malang/" target="_blank" class="mx-2 text-dark"><i class="bi bi-instagram"></i></a>
+    <a href="https://x.com/arvegatu371" target="_blank" class="mx-2 text-dark"><i class="bi bi-twitter-x"></i></a>
+    <a href="https://www.youtube.com/@smpn4malangofficial479" target="_blank" class="mx-2 text-dark"><i class="bi bi-youtube"></i></a>
+  </div>
+
+  <!-- Contact Information -->
+  <div class="container text-center mt-3">
+    <div class="row justify-content-center">
+      <div class="p-2 col-auto d-flex align-items-center" style="font-size: 0.8vw;"><i class="bi bi-geo-alt"></i> Jl. Contoh Alamat No.123, Kota ABC</div>
+      <div class="p-1 col-auto d-flex align-items-center" style="font-size: 0.8vw;"><i class="bi bi-telephone-fill"></i> +62 812 3456 7890</div>
+      <div class="p-1 col-auto d-flex align-items-center" style="font-size: 0.8vw;"><i class="bi bi-envelope-fill"></i> email@example.com</div>
+    </div>
+  </div>
+
+  <!-- Footer -->
+  <footer class="bg-dark text-white text-center">
+    <p class="p-3" style="font-size: 0.9vw; margin-bottom: -1rem">All Rights Reserved</p>
+  </footer>
+</div>
+</body>
+</html>
